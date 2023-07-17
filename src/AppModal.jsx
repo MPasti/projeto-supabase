6ea24a46df9068a82123aa6373c311c2d40a4ce7
MPasti <matheusspasti@gmail.com>
2023-07-17 15:03:06 -0300

    modificações no modal

--- src/AppModal.jsx
+++ src/AppModal.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Modal, ButtonToolbar, Button, Placeholder } from "rsuite";
+import { Modal, ButtonToolbar, Button, Placeholder, Divider } from "rsuite";
 import { AppContext } from "./context/AppContext";
 
 const AppModal = () => {
@@ -22,7 +22,7 @@ const AppModal = () => {
         <Radio value={true}>true</Radio>
         <Radio value={false}>false</Radio>
       </RadioGroup> */}
-      <hr />
+      <Divider>Formulário</Divider>
       <ButtonToolbar>
         <Button color="cyan" appearance="ghost" onClick={handleOpen}>
           Abrir
